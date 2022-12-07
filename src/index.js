18d80d86f83514aa6234e1b094e671d1d0d10abd
anhnt04101996 <anhanhesbt.com>
2022-12-07 22:46:49 +0700

    config app

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+const port = 3003;
+
+app.use(morgan("combined"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine(
+  ".hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+  // res.send("Hello World!")
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
