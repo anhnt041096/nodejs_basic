459e3c816ff1309b15a0e15886e82a532ef4d427
anhnt04101996 <anhanhesbt.com>
2022-12-11 15:27:40 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ const path = require("path");
 const app = express();
 const port = 3003;
 
+const route = require("./routes");
+
 app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
 app.engine(
   ".hbs",
@@ -19,14 +28,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-  // res.send("Hello World!")
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
