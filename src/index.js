dcaccc4924d0f5d8f35e68445b00dbf34ac8fad6
anhnt04101996 <anhanhesbt.com>
2022-12-13 23:21:27 +0700

    build project with MVC model and add lib

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 3003;
 
-const route = require("./routes");
+const route = require('./routes');
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
 app.engine(
-  ".hbs",
-  handlebars.engine({
-    extname: ".hbs",
-  })
+    '.hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
